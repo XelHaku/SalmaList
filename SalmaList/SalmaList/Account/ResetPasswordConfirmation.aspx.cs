303ced39434ff8f547d2732715193e3318bce691
XelHaku <tamez.jm@gmail.com>
2015-01-07 01:00:03 -0600

    Jan 06 Project Creation
    Visual Studio 2013 Web Form

---
+++ SalmaList/SalmaList/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SalmaList.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SalmaList/SalmaList/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalmaList.Startup))]
+namespace SalmaList
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
